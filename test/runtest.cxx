d9945c0917429d97320645c948113954c6412c44
David <d.woodward@gmx.de>
2025-09-07 19:09:37 +0200

    try something

--- test/runtest.cxx
+++ test/runtest.cxx
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <iomanip>
 #include "cutee.h"
+#include "t.codec.h"
 using namespace std;
 using namespace cutee;
 
 // static vars initialization
 CuteeTest* TestList::list[MAX_TEST_COUNT];
-int TestList::list_idx = 0;
+int TestList::list_idx = 1;
 
 int main(int argc, char **argv) 
 {
+
+	TestList::list[TestList::list_idx] = new test_codec();
+	TestList::list_idx++;
+
 	Runner r(argc, argv);
 	r.run();
 }
