63e26dc57941484b7e863420c6efb29fa4cc8dc2
David <d.woodward@gmx.de>
2025-09-25 22:13:34 +0200

    add config.h.in + configure helper module

---
+++ cmake/ConfigureHeader.cmake
@@ -0,0 +1,17 @@
+include(CheckIncludeFile)
+include(CheckIncludeFileCXX)
+
+check_include_file("dirent.h" HAVE_DIRENT_H)
+check_include_file("dlfcn.h" HAVE_DLFCN_H)
+check_include_file("inttypes.h" HAVE_INTTYPES_H)
+check_include_file("memory.h" HAVE_MEMORY_H)
+check_include_file("ndir.h" HAVE_NDIR_H)
+check_include_file("stdint.h" HAVE_STDINT_H)
+check_include_file("stdlib.h" HAVE_STDLIB_H)
+check_include_file("strings.h" HAVE_STRINGS_H)
+check_include_file("string.h" HAVE_STRING_H)
+check_include_file("sys/dir.h" HAVE_SYS_DIR_H)
+check_include_file("sys/ndir.h" HAVE_SYS_NDIR_H)
+
+
+configure_file(${CMAKE_CURRENT_LIST_DIR}/mimetic/config.h.in ${CMAKE_CURRENT_BINARY_DIR}/mimetic/config.h)
